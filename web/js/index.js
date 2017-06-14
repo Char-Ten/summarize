243619c4ac54233b96a5e4227e0d7e08b6623b9b
Charten <645070346@qq.com>
2017-06-14 17:35:09 +0800

    test

--- web/js/index.js
+++ web/js/index.js
@@ -1,135 +1,131 @@
-/**echarts 图表 */
+// var tooltipTpl = document.getElementById('tooltipTpl').innerHTML
+// var opt = {
+//     legend: {
+//         orient: 'vertical',
+//         right: 0,
+//         padding: 0,
+//         itemGap: 1,
+//         tooltip: {
+//             show: false
+//         },
+//         data: [{
+//             name: '当地平均价格',
+//             textStyle: {
+//                 color: '#2E91E6',
+//             },
+//         }, {
+//             name: '全国平均价格',
+//             textStyle: {
+//                 color: '#A0E227'
+//             }
+//         }]
+//     },
+//     xAxis: [{
+//         type: 'category',
+//         name: '日期',
+//         splitNumber: 5,
+//         boundaryGap: false,
+//         data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+//         axisLine: {
+//             lineStyle: {
+//                 color: '#dbdbdb'
+//             }
+//         }
+//     }],
+//     yAxis: [{
+//         type: 'value',
+//         splitNumber: 2,
+//         scale: true,
+//         splitLine: {
+//             show: false
+//         },
+//         axisLine: {
+//             show: false,
+//             lineStyle: {
+//                 color: '#dbdbdb'
+//             }
+//         },
+//         axisTick: {
+//             show: false
+//         }
+//     }],
+//     grid: {
+//         top: '20%',
+//         left: '3%',
+//         right: '4%',
+//         bottom: '25%',
+//         containLabel: true
+//     },
+//     tooltip: {
+//         trigger: 'axis',
+//         backgroundColor: null,
+//         borderWidth: 0,
+//         padding: 0,
+//         textStyle: {
+//             color: '#505050'
+//         },
+//         formatter: function(obj) {
+//             var val0 = '';
+//             var val1 = '';
+//             for (var i = 0; i < obj.length; i++) {
+//                 if (obj[i].seriesName === '当地平均价格') {
+//                     val0 = obj[i].value;
+//                 } else {
+//                     val1 = obj[i].value;
+//                 }
+//             }
+//             return tooltipTpl
+//                 .replace('{0}', val0)
+//                 .replace('{1}', val1)
+//         }
+//     },
+//     dataZoom: {
+//         start: 0,
+//         end: 50
+//     },
+//     series: [{
+//         name: '当地平均价格',
+//         type: 'line',
+//         areaStyle: {
+//             normal: {
+//                 color: 'rgba(0,136,228,0.2)'
+//             }
+//         },
+//         itemStyle: {
+//             normal: {
+//                 color: '#2E91E6'
+//             }
+//         },
+//         lineStyle: {
+//             normal: {
+//                 width: 1
+//             }
+//         },
+//         smooth: true,
+//         data: [10, 25, 15, 45, 68, 22, 33]
+//     }, {
+//         name: '全国平均价格',
+//         type: 'line',
+//         areaStyle: {
+//             normal: {
+//                 color: 'rgba(157,225,14,0.2)'
+//             }
+//         },
+//         itemStyle: {
+//             normal: {
+//                 color: '#A0E227'
+//             }
+//         },
+//         lineStyle: {
+//             normal: {
+//                 width: 1
+//             }
+//         },
+//         smooth: true,
+//         data: [10, 25, 15, 45, 55, 22, 33]
+//     }]
 
-var Eet = new Vue();
-var tooltipTpl = document.getElementById('tooltipTpl').innerHTML
-var opt = {
-    legend: {
-        orient: 'vertical',
-        right: 0,
-        padding: 0,
-        itemGap: 1,
-        tooltip: {
-            show: false
-        },
-        data: [{
-            name: '当地平均价格',
-            textStyle: {
-                color: '#2E91E6',
-            },
-        }, {
-            name: '全国平均价格',
-            textStyle: {
-                color: '#A0E227'
-            }
-        }]
-    },
-    xAxis: [{
-        type: 'category',
-        name: '日期',
-        splitNumber: 5,
-        boundaryGap: false,
-        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-        axisLine: {
-            lineStyle: {
-                color: '#dbdbdb'
-            }
-        }
-    }],
-    yAxis: [{
-        type: 'value',
-        splitNumber: 2,
-        scale: true,
-        splitLine: {
-            show: false
-        },
-        axisLine: {
-            show: false,
-            lineStyle: {
-                color: '#dbdbdb'
-            }
-        },
-        axisTick: {
-            show: false
-        }
-    }],
-    grid: {
-        top: '20%',
-        left: '3%',
-        right: '4%',
-        bottom: '25%',
-        containLabel: true
-    },
-    tooltip: {
-        trigger: 'axis',
-        backgroundColor: null,
-        borderWidth: 0,
-        padding: 0,
-        textStyle: {
-            color: '#505050'
-        },
-        formatter: function(obj) {
-            var val0 = '';
-            var val1 = '';
-            for (var i = 0; i < obj.length; i++) {
-                if (obj[i].seriesName === '当地平均价格') {
-                    val0 = obj[i].value;
-                } else {
-                    val1 = obj[i].value;
-                }
-            }
-            return tooltipTpl
-                .replace('{0}', val0)
-                .replace('{1}', val1)
-        }
-    },
-    dataZoom: {
-        start: 0,
-        end: 50
-    },
-    series: [{
-        name: '当地平均价格',
-        type: 'line',
-        areaStyle: {
-            normal: {
-                color: 'rgba(0,136,228,0.2)'
-            }
-        },
-        itemStyle: {
-            normal: {
-                color: '#2E91E6'
-            }
-        },
-        lineStyle: {
-            normal: {
-                width: 1
-            }
-        },
-        smooth: true,
-        data: [10, 25, 15, 45, 68, 22, 33]
-    }, {
-        name: '全国平均价格',
-        type: 'line',
-        areaStyle: {
-            normal: {
-                color: 'rgba(157,225,14,0.2)'
-            }
-        },
-        itemStyle: {
-            normal: {
-                color: '#A0E227'
-            }
-        },
-        lineStyle: {
-            normal: {
-                width: 1
-            }
-        },
-        smooth: true,
-        data: [10, 25, 15, 45, 55, 22, 33]
-    }]
-
-}
-
+// }
 ;
 (function() {
     // Vue.component('e-charts',{
@@ -144,7 +140,7 @@ var opt = {
     //         chart.setOption(opt);
     //     }
     // });
-
+    alert(1);
     Vue.component('page-price', {
         template: '#priceChartPageTpl',
         data: function() {
