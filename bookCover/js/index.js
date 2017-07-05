1345deee659b3bd977ac5acca96109e9022f6b38
CharTen <645070346@qq.com>
2017-07-05 21:22:23 +0800

    update

--- bookCover/js/index.js
+++ bookCover/js/index.js
@@ -21,20 +21,47 @@
         computed: {
             fillColor: function() {
                 return this.info.color
+            },
+            parseTitle: function() {
+                return this.info.title.split('\\n');
             }
         },
         methods: {
-            draw: function() {
-                this.ctx.fillStyle = this.fillColor
+            render: function() {
+                this.ctx.fillStyle = this.fillColor;
                 this.ctx.clearRect(0, 0, 600, 840);
-                this.ctx.fillRect(30, 0, 540, 30);
+                this.ctx.fillRect(25.2, 0, 554.4, 14.4);
+                this.ctx.fillRect(25.2, 481.2, 554.4, 37.2);
             },
+            renderTitle: function(txt) {
+                this.ctx.save();
+                this.font = "73.2px sans-serif";
+                this.parseTitle.forEach(function() {
+
+                })
+
+                this.ctx.restore();
+            },
+            renderLBTxt: function(txt) {
+
+            },
+            renderRBTxt: function(txt) {
+
+            }
 
         },
         mounted: function() {
             this.cvs = this.$refs['cvs'];
             this.ctx = this.cvs.getContext('2d');
-            this.draw();
+            this.render();
         }
     })
 })();
+
+function W(n) {
+    return 600 * n / 500
+}
+
+function H(n) {
+    return 840 * n / 700
+}
