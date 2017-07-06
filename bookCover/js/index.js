c22cbd52040f2ebc47658c76bca849c3056909dd
Charten <645070346@qq.com>
2017-07-06 12:13:51 +0800

    update

--- bookCover/js/index.js
+++ bookCover/js/index.js
@@ -24,28 +24,53 @@
             },
             parseTitle: function() {
                 return this.info.title.split('\\n');
-            }
+            },
         },
         methods: {
             render: function() {
-                this.ctx.fillStyle = this.fillColor;
                 this.ctx.clearRect(0, 0, 600, 840);
+                this.ctx.fillStyle = '#fff';
+                this.ctx.fillRect(0, 0, 600, 840);
+                this.ctx.fillStyle = this.fillColor;
                 this.ctx.fillRect(25.2, 0, 554.4, 14.4);
-                this.ctx.fillRect(25.2, 481.2, 554.4, 37.2);
+                this.ctx.fillRect(25.2, 481.2, 554.4, 80 * this.parseTitle.length);
+                this.renderTitle();
+                this.renderAuthor();
+                this.renderTopText();
+                this.renderImage();
             },
-            renderTitle: function(txt) {
+            renderTitle: function() {
+                var self = this;
                 this.ctx.save();
-                this.font = "73.2px sans-serif";
-                this.parseTitle.forEach(function() {
-
-                })
-
+                this.ctx.font = "73.2px sans-serif";
+                this.parseTitle.forEach(function(item, i) {
+                    self.ctx.fillStyle = '#fff';
+                    self.ctx.fillText(item, 35.5, 553.7 + i * 73.2, 520.4)
+                });
                 this.ctx.restore();
             },
-            renderLBTxt: function(txt) {
-
+            renderAuthor: function() {
+                this.ctx.save();
+                this.ctx.font = "25.2px Comic Sans MS"
+                this.ctx.fillStyle = '#000';
+                this.ctx.textAlign = "end";
+                this.ctx.fillText(this.info.author, 574.8, 506.4 + this.parseTitle.length * 80)
+                this.ctx.restore();
             },
-            renderRBTxt: function(txt) {
+            renderTopText: function() {
+                this.ctx.save();
+                this.ctx.font = "18px Courier New";
+                this.ctx.textAlign = "center";
+                this.ctx.fillText(this.info.topTxt, 300, 14.4 + 18)
+                this.ctx.restore();
+            },
+            renderImage: function() {
+                var img = document.createElement('img');
+                var self = this
+                img.onload = function() {
+                    self.ctx.drawImage(img, 100, 100, 420, 400)
+                }
+                img.src = '../img/animal2.png';
 
             }
 
